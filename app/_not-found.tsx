e359e8e0a22d6865c2e345e34773952afe025e0b
Leoooii <lioneh39@gmail.com>
2024-12-09 12:40:46 +0200

    Laptop comit - 09.12 11

--- app/_not-found.tsx
+++ app/_not-found.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const _NotFound = ({ children }) => {
+const _NotFound = ({ children }: { children: React.ReactNode }) => {
   return <div>_NotFound {children}</div>
 }
 
