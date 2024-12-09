a28cbe532035a32e53b1b85a4c70f4be0cdd1ec3
Leoooii <lioneh39@gmail.com>
2024-12-09 12:18:13 +0200

    Laptop comit - 09.12 6

--- components/forms/Modal.tsx
+++ components/forms/Modal.tsx
@@ -1,75 +1,75 @@
-'use client'
-
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-} from '@nextui-org/react'
-import React from 'react'
-
-import AddItemForm from '@/components/forms/AddItemForm'
-
-interface ModalProps {
-  onSubmit: () => void
-}
-
-const ModalComponent: React.FC<ModalProps> = ({ onSubmit }) => {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure()
-
-  return (
-    <>
-      <Button color="primary" onPress={onOpen}>
-        Adauga produs
-      </Button>
-      <Modal
-        backdrop="opaque"
-        isOpen={isOpen}
-        motionProps={{
-          variants: {
-            enter: {
-              y: -20,
-              opacity: 1,
-              transition: {
-                duration: 0.6,
-                ease: 'circOut',
-              },
-            },
-            exit: {
-              y: 20,
-              opacity: 0,
-              transition: {
-                duration: 0.2,
-                ease: 'easeIn',
-              },
-            },
-          },
-        }}
-        onOpenChange={onOpenChange}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Adaugare item
-              </ModalHeader>
-              <ModalBody>
-                <AddItemForm onSubmit={onSubmit} />
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Inchide
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
-  )
-}
-
-export default ModalComponent
+// 'use client'
+//
+// import {
+//   Button,
+//   Modal,
+//   ModalBody,
+//   ModalContent,
+//   ModalFooter,
+//   ModalHeader,
+//   useDisclosure,
+// } from '@nextui-org/react'
+// import React from 'react'
+//
+// import AddItemForm from '@/components/forms/AddItemForm'
+//
+// interface ModalProps {
+//   onSubmit: () => void
+// }
+//
+// const ModalComponent: React.FC<ModalProps> = ({ onSubmit }) => {
+//   const { isOpen, onOpen, onOpenChange } = useDisclosure()
+//
+//   return (
+//     <>
+//       <Button color="primary" onPress={onOpen}>
+//         Adauga produs
+//       </Button>
+//       <Modal
+//         backdrop="opaque"
+//         isOpen={isOpen}
+//         motionProps={{
+//           variants: {
+//             enter: {
+//               y: -20,
+//               opacity: 1,
+//               transition: {
+//                 duration: 0.6,
+//                 ease: 'circOut',
+//               },
+//             },
+//             exit: {
+//               y: 20,
+//               opacity: 0,
+//               transition: {
+//                 duration: 0.2,
+//                 ease: 'easeIn',
+//               },
+//             },
+//           },
+//         }}
+//         onOpenChange={onOpenChange}
+//       >
+//         <ModalContent>
+//           {(onClose) => (
+//             <>
+//               <ModalHeader className="flex flex-col gap-1">
+//                 Adaugare item
+//               </ModalHeader>
+//               <ModalBody>
+//                 <AddItemForm onSubmit={onSubmit} />
+//               </ModalBody>
+//               <ModalFooter>
+//                 <Button color="danger" variant="flat" onPress={onClose}>
+//                   Inchide
+//                 </Button>
+//               </ModalFooter>
+//             </>
+//           )}
+//         </ModalContent>
+//       </Modal>
+//     </>
+//   )
+// }
+//
+// export default ModalComponent
