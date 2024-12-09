20064b917571620975be480233ff2abd9ac3f80b
Leoooii <lioneh39@gmail.com>
2024-12-09 11:01:56 +0200

    Laptop comit - 09.12 tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,41 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
+// import type { Config } from 'tailwindcss';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// module.exports = {
+//   content: [
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["var(--font-sans)"],
+//         mono: ["var(--font-mono)"],
+//       },
+//     },
+//   },
+//   darkMode: "class",
+//  plugins: [nextui()],
+// }
+
+const config = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-      },
-    },
+        mono: ["var(--font-mono)"]
+      }
+    }
   },
   darkMode: "class",
- plugins: [nextui()],
-}
+  plugins: [nextui()]
+};
+
+export default config;
